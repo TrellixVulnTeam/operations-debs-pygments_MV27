1f86c722688d3b820daadf794bb01f6a4360e8f8
SVN-Git Migration <python-modules-team@lists.alioth.debian.org>
2015-10-08 10:34:41 -0700

    Imported Upstream version 1.2.2+dfsg

--- pygments/__init__.py
+++ pygments/__init__.py
@@ -12,7 +12,8 @@
     * a wide range of common languages and markup formats is supported
     * special attention is paid to details, increasing quality by a fair amount
     * support for new languages and formats are added easily
-    * a number of output formats, presently HTML, LaTeX, RTF, SVG and ANSI sequences
+    * a number of output formats, presently HTML, LaTeX, RTF, SVG, all image
+      formats that PIL supports, and ANSI sequences
     * it is usable as a command-line tool and as a library
     * ... and it highlights even Brainfuck!
 
@@ -25,7 +26,7 @@
     :license: BSD, see LICENSE for details.
 """
 
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 __docformat__ = 'restructuredtext'
 
 __all__ = ['lex', 'format', 'highlight']
