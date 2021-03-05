62ce0110fd916d8da08e00aa58baa00687bd6956
Kunal Mehta <legoktm@debian.org>
2021-03-05 14:34:31 -0800

    New upstream version 2.8.0+dfsg

--- pygments/lexers/theorem.py
+++ pygments/lexers/theorem.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 """
     pygments.lexers.theorem
     ~~~~~~~~~~~~~~~~~~~~~~~
 
     Lexers for theorem-proving languages.
 
-    :copyright: Copyright 2006-2020 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
@@ -29,6 +28,8 @@ class CoqLexer(RegexLexer):
     aliases = ['coq']
     filenames = ['*.v']
     mimetypes = ['text/x-coq']
+
+    flags = re.UNICODE
 
     keywords1 = (
         # Vernacular commands
@@ -123,14 +124,15 @@ class CoqLexer(RegexLexer):
             (r'0[bB][01][01_]*', Number.Bin),
             (r'-?\d[\d_]*(.[\d_]*)?([eE][+\-]?\d[\d_]*)', Number.Float),
 
-            (r"'(?:(\\[\\\"'ntbr ])|(\\[0-9]{3})|(\\x[0-9a-fA-F]{2}))'",
-             String.Char),
+            (r"'(?:(\\[\\\"'ntbr ])|(\\[0-9]{3})|(\\x[0-9a-fA-F]{2}))'", String.Char),
+
             (r"'.'", String.Char),
             (r"'", Keyword),  # a stray quote is another syntax element
 
             (r'"', String.Double, 'string'),
 
             (r'[~?][a-z][\w\']*:', Name),
+            (r'\S', Name.Builtin.Pseudo),
         ],
         'comment': [
             (r'[^(*)]+', Comment),
@@ -154,8 +156,8 @@ class CoqLexer(RegexLexer):
     }
 
     def analyse_text(text):
-        if text.startswith('(*'):
-            return True
+        if 'Qed' in text and 'Proof' in text:
+            return 1
 
 
 class IsabelleLexer(RegexLexer):
@@ -410,6 +412,7 @@ class LeanLexer(RegexLexer):
                 'universe', 'universes',
                 'inductive', 'coinductive', 'structure', 'extends',
                 'class', 'instance',
+                'abbreviation',
 
                 'noncomputable theory',
 
@@ -434,6 +437,7 @@ class LeanLexer(RegexLexer):
                 'let', 'if', 'else', 'then', 'in', 'with', 'calc', 'match',
                 'do'
             ), prefix=r'\b', suffix=r'\b'), Keyword),
+            (words(('sorry', 'admit'), prefix=r'\b', suffix=r'\b'), Generic.Error),
             (words(('Sort', 'Prop', 'Type'), prefix=r'\b', suffix=r'\b'), Keyword.Type),
             (words((
                 '#eval', '#check', '#reduce', '#exit',
